96b71f54c739e65dd308f4c893bd2e77ade66dd0
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2020-06-17 12:09:03 +0300

    version 1.1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.0',
+    version='1.1.1',
 
     description='The amazing Murre will normalize non-standard Finnish and dialectalize standard Finnish!',
     long_description=long_description,
@@ -69,7 +69,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["natas", "OpenNMT-tf", "pyonmttok"],
+    install_requires=["natas", "OpenNMT-tf", "pyonmttok==1.10.1"],
    # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
