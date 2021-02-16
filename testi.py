57879422271e3d7f0c92a071675c412f23986949
Mika <mika.hamalainen@helsinki.fi>
2021-02-16 13:56:27 +0200

    agricola works

--- testi.py
+++ testi.py
@@ -14,8 +14,8 @@ print(normalize_sentences(sentences))
 #print(generator._translate(["Etelä-Häme k i s s a n i _ j u o k s i _ p o i s"]))
 
 
-sents = ["minä nauran ja kovaa", "hattu on päässä aina talvisin"]
-sentences = [x.split(" ") for x in sents]
+#sents = ["minä nauran ja kovaa", "hattu on päässä aina talvisin"]
+#sentences = [x.split(" ") for x in sents]
 
 """
 
@@ -27,6 +27,8 @@ print(supported_dialects())
 
 """
 
-for dialect in supported_dialects():
-	print(dialect)
-	print(dialectalize_sentences(sentences, dialect))
+#for dialect in supported_dialects():
+#	print(dialect)
+#	print(dialectalize_sentences(sentences, dialect))
+
+print(normalize_sentence("wanha ia aika", language="fin_hist"))
